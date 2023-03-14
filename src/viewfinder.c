4b3e40d15f46e017bb729f96546873f4788297ae
cuongmzq <63182706+cuongmzq@users.noreply.github.com>
2023-03-14 23:58:47 +0700

    Update viewfinder.c

--- src/viewfinder.c
+++ src/viewfinder.c
@@ -1065,12 +1065,12 @@ int mmal_start_camcorder(volatile int *stop, MMALCAM_BEHAVIOUR_T *behaviour, on_
     int is_start = 0;
    while(1)
    {
-    if (!is_start) {
+    if (is_start < 2) {
         if (mmal_port_parameter_set_boolean(encoder_output, MMAL_PARAMETER_VIDEO_REQUEST_I_FRAME, 1) != MMAL_SUCCESS)
         {
             vcos_log_error("failed to request I-FRAME");
         }
-        is_start = 1;
+        is_start++;
     }
     
       MMAL_BUFFER_HEADER_T *buffer;
